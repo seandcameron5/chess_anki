75755bf1076730126bc331bfb3591c12b93c59e4
seandcameron5 <seandcameron5@gmail.com>
2024-12-29 15:55:54 -0500

    yo this is my first commit

---
+++ svg_maker.py
@@ -0,0 +1,24 @@
+import chess
+import chess.svg
+import chess.pgn
+
+
+# is this pgn for Black or White (which side are you learning?)
+forWhite = False
+
+# opening pgn, setting up board
+pgn = open("Spanish_vs_for Black_____.__.__.pgn")
+game = chess.pgn.read_game(pgn)
+board = game.board()
+
+#list of svgs
+svgs = []
+mainline_moves_count = 0
+for move in game.mainline_moves():
+    mainline_moves_count += 1
+    board.push(move)
+    svgs.append(chess.svg.board(board,size = 350))
+
+for i in range(5, mainline_moves_count):
+    with open(f"spanish_test{i}.svg", "w") as f:
+        f.write(svgs[i])
