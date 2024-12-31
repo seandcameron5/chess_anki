a80f7fed271565f3f4a301477a35be3230fe383d
seandcameron5 <seandcameron5@gmail.com>
2024-12-30 23:53:44 -0500

    tried making recursive search work, it did not :(

--- svg_maker.py
+++ svg_maker.py
@@ -7,10 +7,10 @@ import chess.pgn
 playing_as = chess.BLACK
 
 #opening name
-opening = 'spanish'
+opening = 'italian'
 
 # opening pgn, setting up board
-pgn = open("Spanish_vs_for Black_____.__.__.pgn")
+pgn = open("__vs_______.__.__.pgn")
 game = chess.pgn.read_game(pgn)
 board = game.board()
 
@@ -26,8 +26,34 @@ for move in game.mainline_moves():
     svgs.append(chess.svg.board(board,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
     mainline_moves_count += 1
 
-# new way of iterating through: use nodes in order to more easily access variations.
-#for n in game.mainline():
+
+# creates a list containing two SVGs for the front and back of a card
+def create_svg(board1, board2):
+    svgs = []
+    svgs.append(chess.svg.board(board1,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
+    svgs.append(chess.svg.board(board2,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
+    return svgs
+
+# recursively seraches through game, creating svgs
+def goThroughGame(notes, node):
+    print(node.board())
+    for v in node.variations:
+        if node.turn() == playing_as and node.ply() > 1:
+            notes.append(create_svg(node.board(), v.board()))
+        return goThroughGame(notes, v)
+    print('returning notes')
+    return notes
+
+notes = goThroughGame([], game)
+print(len(notes))
+
+for i in range(len(notes)):
+    with open(f"{opening}{i}w.svg", "w") as f:
+        f.write(notes[i][0])
+    with open(f"{opening}{i}b.svg", "w") as f:
+        f.write(notes[i][1])
+
+
 
 
 for i in range(0, 6):
