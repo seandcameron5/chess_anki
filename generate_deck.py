4cd17d84e6cf6b519d91bfdad316c7b36bf1fdd9
seandcameron5 <seandcameron5@gmail.com>
2025-01-03 00:31:04 -0500

    Finished project.

--- generate_deck.py
+++ generate_deck.py
@@ -1,6 +1,25 @@
 import os
+import json
+import urllib.request
 
 CURRENT_DIRECTORY = os.getcwd()
+
+
+def request(action, **params):
+    return {'action': action, 'params': params, 'version': 6}
+
+def invoke(action, **params):
+    requestJson = json.dumps(request(action, **params)).encode('utf-8')
+    response = json.load(urllib.request.urlopen(urllib.request.Request('http://127.0.0.1:8765', requestJson)))
+    if len(response) != 2:
+        raise Exception('response has an unexpected number of fields')
+    if 'error' not in response:
+        raise Exception('response is missing required error field')
+    if 'result' not in response:
+        raise Exception('response is missing required result field')
+    if response['error'] is not None:
+        raise Exception(response['error'])
+    return response['result']
 
 def generate_params(deckName: str, fields: list[str], file_names: list[str], field_texts: list[str] = None):
     return {
@@ -32,5 +51,13 @@ def generate_params(deckName: str, fields: list[str], file_names: list[str], fie
         }
     }
 
+opening = "Spanish"
+invoke('createDeck', deck=opening)
 
-print(generate_params(deckName="TestDeck", fields=["Front", "Back"], file_names=["normal_file1", "normal_file2"]))
+
+params = []
+for i in range(69):
+    params.append(generate_params(deckName=opening, fields=["Front", "Back"], file_names=[opening+f'{i}'+'f.svg', opening+f'{i}'+'b.svg']))
+
+for p in params:
+    invoke('addNote', **p)
--- svg_maker.py
+++ svg_maker.py
@@ -2,64 +2,37 @@ import chess
 import chess.svg
 import chess.pgn
 
-
 # is this pgn for Black or White (which side are you learning?)
 playing_as = chess.BLACK
 
 #opening name
-opening = 'italian'
+opening = 'Spanish'
 
 # opening pgn, setting up board
-pgn = open("__vs_______.__.__.pgn")
+pgn = open("Spanish_vs_for Black_____.__.__.pgn")
 game = chess.pgn.read_game(pgn)
-board = game.board()
-
-#list of svgs
-svgs = []
-svgs_variations = []
 board_orientation = playing_as
 
-#generates svg's for mainline
-mainline_moves_count = 0
-for move in game.mainline_moves():
-    board.push(move)
-    svgs.append(chess.svg.board(board,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
-    mainline_moves_count += 1
-
-
 # creates a list containing two SVGs for the front and back of a card
-def create_svg(board1, board2):
+def create_svg(board1, board2, move1, move2):
     svgs = []
-    svgs.append(chess.svg.board(board1,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
-    svgs.append(chess.svg.board(board2,lastmove=move,orientation=board_orientation,coordinates=False, size = 350))
+    svgs.append(chess.svg.board(board1,lastmove=move1,orientation=board_orientation,coordinates=False, size = 500))
+    svgs.append(chess.svg.board(board2,lastmove=move2,orientation=board_orientation,coordinates=False, size = 500))
     return svgs
 
 # recursively seraches through game, creating svgs
 def goThroughGame(notes, node):
-    print(node.board())
     for v in node.variations:
-        if node.turn() == playing_as and node.ply() > 1:
-            notes.append(create_svg(node.board(), v.board()))
+        if node.turn() == playing_as and node.ply() > 6:
+            notes.append(create_svg(node.board(), v.board(), node.move, v.move))
         notes = goThroughGame(notes, v)
-    print('returning notes')
     return notes
 
 notes = goThroughGame([], game)
 print(len(notes))
 
 for i in range(len(notes)):
-    with open(f"{opening}{i}w.svg", "w") as f:
+    with open(f"{opening}{i}f.svg", "w") as f:
         f.write(notes[i][0])
     with open(f"{opening}{i}b.svg", "w") as f:
         f.write(notes[i][1])
-
-
-
-
-for i in range(0, 6):
-    if i % 2 == 0:
-        with open(f"{opening}{i // 2 + 1}w.svg", "w") as f:
-            f.write(svgs[i])
-    else:
-        with open(f"{opening}{i // 2 + 1}b.svg", "w") as f:
-            f.write(svgs[i])
